a10636437eaa9c2e4145f467bbcf1a8dd4cedeef
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-12-03 18:20:11 +0000

    create Overview comp + css files

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,11 @@
 import "../css/Hero.css";
+import Overview from "./Overview";
 
 export default function Hero() {
-  return <div className="hero-container"></div>;
+  return (
+    <>
+      <div className="hero-container"></div>
+      <Overview />
+    </>
+  );
 }
---
+++ src/components/Overview.jsx
@@ -0,0 +1,5 @@
+import "../css/Overview.css";
+
+export default function Overview() {
+  return <div className="overview-container"></div>;
+}
