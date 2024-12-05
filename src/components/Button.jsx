2fd70bcb287c1bb4ccc4ada8e48e1108b9563859
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-12-05 22:54:24 +0000

    create button comp and design it

---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+import "../css/Button.css";
+
+export default function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
--- src/components/WhyFreestyle.jsx
+++ src/components/WhyFreestyle.jsx
@@ -1,13 +1,20 @@
 import "../css/WhyFreestyle.css";
 import Bullet from "./Bullet";
+import Button from "./Button";
 
 export default function WhyFreestyle() {
   return (
     <div className="why-freestyle-container">
-      <div className="left">
+      <div className="freestyle-left">
         <Bullet>Training Benefits</Bullet>
+        <span>
+          <Button>Benefits</Button>
+          <Button>Perks</Button>
+          <Button>Advantages</Button>
+          <Button>Extras</Button>
+        </span>
       </div>
-      <div className="right"></div>
+      <div className="freestyle-right"></div>
     </div>
   );
 }
